f8db9c18c58b1d8d7e3a988407083faeab4fe405
Andrea Fagiani <andfagiani@gmail.com>
2016-11-28 12:08:54 +0100

    tmux: attach support; if there is an already  established terminator session, attach to it, otherwise make a new one

--- terminatorlib/terminator.py
+++ terminatorlib/terminator.py
@@ -10,7 +10,7 @@ from gi.repository import Gtk, Gdk
 from borg import Borg
 from config import Config
 from keybindings import Keybindings
-from util import dbg, err, enumerate_descendants, TMUX_ATTACH
+from util import dbg, err, enumerate_descendants, TMUX
 from factory import Factory
 from cwd import get_pid_cwd
 from version import APP_NAME, APP_VERSION
@@ -95,13 +95,11 @@ class Terminator(Borg):
             self.attempt_gnome_client()
         if self.pane_id_to_terminal is None:
             self.pane_id_to_terminal = {}
-        if self.tmux_control is None:
+        if self.tmux_control is None and TMUX:
             handler = tmux.notifications.NotificationsHandler(self)
             self.tmux_control = tmux.control.TmuxControl(
                 session_name='terminator',
                 notifications_handler=handler)
-            if TMUX_ATTACH:
-                self.tmux_control.attach_session()
 
     def set_origcwd(self, cwd):
         """Store the original cwd our process inherits"""
@@ -110,9 +108,10 @@ class Terminator(Borg):
             os.chdir(cwd)
         self.origcwd = cwd
 
-    def set_tmux_remote(self, remote):
-        """Store the command line argument intended for tmux"""
+    def start_tmux(self, remote=None):
+        """Store the command line argument intended for tmux and start the process"""
         self.tmux_control.remote = remote
+        self.tmux_control.attach_session()
 
     def set_dbus_data(self, dbus_service):
         """Store the DBus bus details, if they are available"""
