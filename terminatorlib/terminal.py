44c925f11369b0ee0622f9eefebf29309e937969
Andrea Fagiani <andfagiani@gmail.com>
2016-12-03 13:57:54 +0100

    tmux: improved zoom support, fixed special characters

--- terminatorlib/terminal.py
+++ terminatorlib/terminal.py
@@ -1808,20 +1808,20 @@ class Terminal(Gtk.VBox):
         self.emit('move-tab', 'left')
 
     def key_toggle_zoom(self):
+        if self.terminator.tmux_control:
+            self.control.toggle_zoom(self.pane_id)
         if self.is_zoomed():
             self.unzoom()
         else:
             self.maximise()
-        if self.terminator.tmux_control:
-            self.control.toggle_zoom(self.pane_id)
 
     def key_scaled_zoom(self):
+        if self.terminator.tmux_control:
+            self.control.toggle_zoom(self.pane_id)
         if self.is_zoomed():
             self.unzoom()
         else:
             self.zoom()
-        if self.terminator.tmux_control:
-            self.control.toggle_zoom(self.pane_id)
 
     def key_next_tab(self):
         self.emit('tab-change', -1)
